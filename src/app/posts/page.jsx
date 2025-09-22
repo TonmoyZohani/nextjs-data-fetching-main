82723060e0b070689bd8204c54285c06aae18099
TonmoyZohani <tonmoyzohani@gmail.com>
2025-09-22 22:25:57 +0600

    get posts

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -12,25 +12,25 @@ export const metadata = {
   description: "Loading JSON placeholder posts using Server Component",
 };
 
-export default async function Posts() {
-  const posts = await getPosts();
-  return (
-    <div className="gap-8 grid grid-cols-4">
-      {posts.map((singlePost) => {
-        return (
-          <div key={singlePost.id} className="border-2 border-slate-300">
-            <p className={`text-2xl font-bold ${style["post-title"]}`}>
-              {singlePost.title}
-            </p>
-            <p className="font-semibold testing-purpose-css-class">
-              {singlePost.body}
-            </p>
-            <Link href={`/posts/${singlePost.id}`}>
-              <button className="bg-green-300 text-black">Details</button>
-            </Link>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+// export default async function Posts() {
+//   const posts = await getPosts();
+//   return (
+//     <div className="gap-8 grid grid-cols-4">
+//       {posts.map((singlePost) => {
+//         return (
+//           <div key={singlePost.id} className="border-2 border-slate-300">
+//             <p className={`text-2xl font-bold ${style["post-title"]}`}>
+//               {singlePost.title}
+//             </p>
+//             <p className="font-semibold testing-purpose-css-class">
+//               {singlePost.body}
+//             </p>
+//             <Link href={`/posts/${singlePost.id}`}>
+//               <button className="bg-green-300 text-black">Details</button>
+//             </Link>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
