915b2fe5e85e24366dc8db43f42d27618a197b20
TonmoyZohani <tonmoyzohani@gmail.com>
2025-09-23 22:29:53 +0600

    meal search input done

--- src/app/meals/components/MealSearchInput.jsx
+++ src/app/meals/components/MealSearchInput.jsx
@@ -2,24 +2,24 @@
 import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
-export default function MealSearchInput() {
-  //   const [meals, setMeals] = useState([]);
-  const [search, setSearch] = useState("");
-  const router = useRouter();
-  const pathname = usePathname();
-  useEffect(() => {
-    const searchQuery = { search };
-    const urlQueryParam = new URLSearchParams(searchQuery);
-    const url = `${pathname}?${urlQueryParam}`;
-    router.push(url);
-  }, [search]);
-  return (
-    <div>
-      <input
-        className="text-black"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-    </div>
-  );
-}
+// export default function MealSearchInput() {
+//   //   const [meals, setMeals] = useState([]);
+//   const [search, setSearch] = useState("");
+//   const router = useRouter();
+//   const pathname = usePathname();
+//   useEffect(() => {
+//     const searchQuery = { search };
+//     const urlQueryParam = new URLSearchParams(searchQuery);
+//     const url = `${pathname}?${urlQueryParam}`;
+//     router.push(url);
+//   }, [search]);
+//   return (
+//     <div>
+//       <input
+//         className="text-black"
+//         value={search}
+//         onChange={(e) => setSearch(e.target.value)}
+//       />
+//     </div>
+//   );
+// }
